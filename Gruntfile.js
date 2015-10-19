4896ea8e7e91cd9507aa9adcaca8737c644fb45c
Matthias Eder <nodeful@gmail.com>
2015-10-19 14:11:11 -0700

    Update Electron to v0.34 which ships with Node.js 4.1.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function(grunt) {
           dir             : '.',
           out             : 'dist',
           icon            : 'cumulus.icns',
-          version         : '0.30.2',
+          version         : '0.34.0',
           platform        : 'darwin',
           arch            : 'x64',
           // ignore          : 'node_modules/',
